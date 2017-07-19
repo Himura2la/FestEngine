e491b0c2231bd2ad2b57f33b2459dad8ec187792
Himura Kazuto <glagol15@gmail.com>
2017-07-19 10:17:01 +0300

    volume magic

--- main.py
+++ main.py
@@ -51,10 +51,6 @@ class MainFrame(wx.Frame):
                   proj_win_menu.Append(wx.ID_ANY, "&Create"))
         self.Bind(wx.EVT_MENU, self.destroy_proj_win,
                   proj_win_menu.Append(wx.ID_ANY, "&Destroy"))
-        self.menu_switch_to_img = proj_win_menu.Append(wx.ID_ANY, "&Switch to Image")
-        self.menu_switch_to_img.Enable(False)
-        self.menu_switch_to_video = proj_win_menu.Append(wx.ID_ANY, "&Switch to Video")
-        self.menu_switch_to_video.Enable(False)
         menu_bar.Append(proj_win_menu, "&Projector Window")
 
         # --- Play ---
@@ -62,11 +58,9 @@ class MainFrame(wx.Frame):
         menu_no_show = menu_play.Append(wx.ID_ANY, "&No Show\tEsc")
         menu_show_zad = menu_play.Append(wx.ID_ANY, "Sho&w zad\tF1")
         menu_play_mp3 = menu_play.Append(wx.ID_ANY, "&Play\tF2")
-        menu_stop_mp3 = menu_play.Append(wx.ID_ANY, "&Fade out")
         self.Bind(wx.EVT_MENU, self.no_show, menu_no_show)
         self.Bind(wx.EVT_MENU, self.show_zad, menu_show_zad)
         self.Bind(wx.EVT_MENU, self.play, menu_play_mp3)
-        self.Bind(wx.EVT_MENU, self.stop, menu_stop_mp3)
         accelerator_table.append(wx.AcceleratorEntry(wx.ACCEL_NORMAL, wx.WXK_ESCAPE, menu_no_show.GetId()))
         accelerator_table.append(wx.AcceleratorEntry(wx.ACCEL_NORMAL, wx.WXK_F1, menu_show_zad.GetId()))
         accelerator_table.append(wx.AcceleratorEntry(wx.ACCEL_NORMAL, wx.WXK_F2, menu_play_mp3.GetId()))
@@ -75,7 +69,28 @@ class MainFrame(wx.Frame):
 
         self.SetMenuBar(menu_bar)
 
+        # ---------------------------------------------- Layout -----------------------------------------------------
         main_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.toolbar = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.toolbar.Add(wx.StaticText(self, label=' VOL '), 0, wx.ALIGN_CENTER_VERTICAL)
+        self.vol_control = wx.SpinCtrl(self, value='-1', size=(50, 20))
+        self.toolbar.Add(self.vol_control, 0)
+        self.vol_control.SetRange(-1, 200)
+        self.Bind(wx.EVT_SPINCTRL, self.set_vol, self.vol_control)
+
+        self.fade_out_btn = wx.Button(self, label="Fade out", size=(100, 20))
+        self.fade_out_btn.Enable(False)
+        self.toolbar.Add(self.fade_out_btn, 0)
+        self.fade_out_btn.Bind(wx.EVT_BUTTON, self.stop)
+
+        self.vid_btn = wx.ToggleButton(self, label='VID', size=(35, 20))
+        self.img_btn = wx.ToggleButton(self, label='IMG', size=(35, 20))
+        self.vid_btn.Enable(False)
+        self.img_btn.Enable(False)
+        self.toolbar.Add(self.vid_btn, 0)
+        self.toolbar.Add(self.img_btn, 0)
 
         # --- Table ---
         self.grid = wx.grid.Grid(self)
@@ -93,6 +108,7 @@ class MainFrame(wx.Frame):
         self.Bind(wx.grid.EVT_GRID_SELECT_CELL, select_row)
         self.Bind(wx.grid.EVT_GRID_RANGE_SELECT, select_row)
 
+        main_sizer.Add(self.toolbar, 0, wx.EXPAND)
         main_sizer.Add(self.grid, 1, wx.EXPAND)
 
         # TODO: Progress bar
@@ -113,10 +129,23 @@ class MainFrame(wx.Frame):
         self.vlc_instance = vlc.Instance()
         self.player = self.vlc_instance.media_player_new()
         self.player.audio_set_volume(100)
+        self.player.audio_set_mute(False)
+        self.vol_control.SetValue(self.player.audio_get_volume())
 
         self.load_data()
 
 # ---------------------------------------------------------------------------------------------------------------------
+
+    def grid_set_shape(self, new_rows, new_cols):
+        current_rows, current_cols = self.grid.GetNumberRows(), self.grid.GetNumberCols()
+        if new_rows < current_rows:
+            self.grid.DeleteRows(0, current_rows - new_rows, True)
+        elif new_rows > current_rows:
+            self.grid.AppendRows(new_rows - current_rows)
+        if new_cols < current_cols:
+            self.grid.DeleteCols(0, current_cols - new_cols, True)
+        elif new_cols > current_cols:
+            self.grid.AppendCols(new_cols - current_cols)
 
     def status(self, text):
         self.status_bar.SetStatusText(text, 0)
@@ -138,32 +167,36 @@ class MainFrame(wx.Frame):
             self.settings = settings_dialog.get_settings()
         settings_dialog.Destroy()
 
-    def grid_set_shape(self, new_rows, new_cols):
-        current_rows, current_cols = self.grid.GetNumberRows(), self.grid.GetNumberCols()
-        if new_rows < current_rows:
-            self.grid.DeleteRows(0, current_rows - new_rows, True)
-        elif new_rows > current_rows:
-            self.grid.AppendRows(new_rows - current_rows)
-        if new_cols < current_cols:
-            self.grid.DeleteCols(0, current_cols - new_cols, True)
-        elif new_cols > current_cols:
-            self.grid.AppendCols(new_cols - current_cols)
+    def switch_to_vid(self, e=None):
+        self.vid_btn.SetValue(True)
+        self.img_btn.SetValue(False)
+        self.proj_win.switch_to_video()
+
+    def switch_to_img(self, e=None):
+        self.vid_btn.SetValue(False)
+        self.img_btn.SetValue(True)
+        self.proj_win.switch_to_images()
 
     # ----------------------------------------------------
 
     def ensure_proj_win(self, e=None):
         if not isinstance(self.proj_win, ProjectorWindow):
             self.proj_win = ProjectorWindow(self, self.settings[Config.PROJECTOR_SCREEN])
-            self.Bind(wx.EVT_MENU, self.proj_win.switch_to_images, self.menu_switch_to_img)
-            self.Bind(wx.EVT_MENU, self.proj_win.switch_to_video, self.menu_switch_to_video)
-            self.menu_switch_to_img.Enable(True)
-            self.menu_switch_to_video.Enable(True)
+
+            self.vid_btn.Bind(wx.EVT_TOGGLEBUTTON, self.switch_to_vid)
+            self.img_btn.Bind(wx.EVT_TOGGLEBUTTON, self.switch_to_img)
+            self.vid_btn.Enable(True)
+            self.img_btn.Enable(True)
+            self.img_btn.SetValue(True)
+
         self.proj_win.Show()
         self.Raise()
 
     def destroy_proj_win(self, e=None):
         if isinstance(self.proj_win, ProjectorWindow):
             self.proj_win.Close(True)
+            self.vid_btn.Enable(False)
+            self.img_btn.Enable(False)
 
     def load_data(self, e=None):
         zad_file_names = os.listdir(zad_path)
@@ -233,7 +266,7 @@ class MainFrame(wx.Frame):
     def no_show(self, e=None):
         if isinstance(self.proj_win, ProjectorWindow):
             self.clear_zad("No show")
-        self.stop(False)
+        self.stop(fade_out=False)
 
     def clear_zad(self, main_status="ZAD Cleared!"):
         if background_zad:
@@ -255,12 +288,21 @@ class MainFrame(wx.Frame):
             self.player.set_media(media)
 
             if self.player.play() != -1:
+                # Playing ...
+                self.fade_out_btn.Enable(True)
+
                 if file_path.rsplit('.', 1)[1] not in {'mp3', 'wav'}:
                     self.ensure_proj_win()
-                    self.proj_win.switch_to_video()
-                time.sleep(0.05)
-                self.player.audio_set_volume(100)
-                self.sound_status("Playing... Vol: %d" % self.player.audio_get_volume())
+                    self.switch_to_vid()
+                    # TODO: Switch back after video ends
+
+                while self.player.audio_get_volume() != self.vol_control.GetValue():
+                    self.player.audio_set_mute(False)
+                    self.player.audio_set_volume(self.vol_control.GetValue())
+                    self.sound_status("Trying to unmute...")
+                    print "Trying to set unmute..."
+                    time.sleep(0.05)
+                self.sound_status("Playing...")
             else:
                 self.sound_status("ERROR PLAYING FILE!!!")
 
@@ -268,15 +310,25 @@ class MainFrame(wx.Frame):
             self.sound_status("Nothing to play for '%s'" % self.items[id]['name'])
 
     def stop(self, e=None, fade_out=True):
+        self.fade_out_btn.Enable(False)
+        self.sound_status("Fading out...")
+        label = self.fade_out_btn.GetLabel()
         if fade_out:
-            for i in range(99, 0, -1):
-                self.player.audio_set_volume(i)
-                self.sound_status("Fading out... Vol: %d" % self.player.audio_get_volume())
+            for i in range(self.player.audio_get_volume(), 0, -1):
+                self.set_vol(vol=i, status='Fading out... ')
                 time.sleep(0.01)
-            self.player.audio_set_volume(100)
-
-        self.sound_status("Stopped. Vol: %d" % self.player.audio_get_volume())
         self.player.stop()
+        self.sound_status("Stopped")
+        self.fade_out_btn.SetLabel(label)
+
+    def set_vol(self, e=None, vol=100, status=''):
+        value = e.Int if e else vol
+        if self.player.audio_set_volume(value) == -1:
+            self.sound_status("Failed to set volume")
+        real_vol = self.player.audio_get_volume()
+        if real_vol < 0:
+            self.player.audio_set_mute(False)
+        self.sound_status(status + "Vol: %d" % real_vol)
 
 if __name__ == "__main__":
     app = wx.App(False)
