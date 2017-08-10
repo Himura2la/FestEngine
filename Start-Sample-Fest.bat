2a6197964b96dd5811111ec62fe0f83059e4b050
Himura Kazuto <glagol15@gmail.com>
2017-08-11 01:44:30 +0300

    readme

--- Start-Sample-Fest.bat
+++ Start-Sample-Fest.bat
@@ -1,8 +1,8 @@
 :: encoding: cp866
 cd src
 start pyw -2 main.pyw ^
-    --filename_re "^(?P<nom>\w{1,2})( \[(?P<start>[GW]{1})\])?\. (?P<name>.*?)(\(.(?P<num>\d{1,3})\))?$" ^
-    --zad_dir "H:\ownCloud\DATA\Прошлые Фесты\Yuki no Odori 2016\Fest\zad_numbered" ^
-    --mp3_dir "H:\ownCloud\DATA\Прошлые Фесты\Yuki no Odori 2016\Fest\mp3_numbered" ^
-    --background_mp3_dir "H:\ownCloud\DATA\Прошлые Фесты\Yuki no Odori 2016\Fest\background" ^
+    --filename_re "(?P<name>.*)" ^
+    --zad_dir "D:\Ваш фестиваль\zad" ^
+    --mp3_dir "D:\Ваш фестиваль\mp3" ^
+    --background_mp3_dir "D:\Ваш фестиваль\background" ^
     --auto_load_files --auto_load_bg
