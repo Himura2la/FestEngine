581c900897d58e705aafaa86f87451dde6c73e1a
Himura Kazuto <glagol15@gmail.com>
2017-10-04 21:49:32 +0300

    Removed vlc logging and wontfixed encoding

--- Debug-hokoritori17.bat
+++
@@ -1,8 +0,0 @@
-:: encoding: cp866
-cd src
-python main.pyw ^
-    --filename_re "^(?P<num>\d{3}) (?P<nom>.*?)\. (?P<nicks>.*?) - (?P<name>.*?)( \(ID(?P<id>\d{1,3})\))?$" ^
-    --background_tracks_dir "E:\Fest\background" ^
-    --background_zad_path "E:\Fest\ht17_bg.jpg" ^
-    --auto_load_files --debug_output
-    "E:\Fest\zad" "E:\Fest\tracks"
---
+++ debug-tulafest16.ps1
@@ -0,0 +1,12 @@
+﻿
+$filename_re = "^(?P<num>\d{3}) (?P<nom>\w{1,2})( \[(?P<start>[GW]{1})\])?\. (?P<name>.*?)(\(.(?P<id>\d{1,3})\))?$"
+$bg_tracks = "D:\Clouds\ownCloud\DATA\Прошлые Фесты\Yuki no Odori 2016\Fest\background"
+$key1 = "--auto_load_files"
+$key2 = "--auto_load_bg"
+$key3 = "--debug_output"
+
+$dir1 = "D:\Clouds\ownCloud\DATA\Прошлые Фесты\Yuki no Odori 2016\Fest\mp3_numbered"
+$dir2 = "D:\Clouds\ownCloud\DATA\Прошлые Фесты\Yuki no Odori 2016\Fest\zad_numbered"
+
+cd src
+& pyw main.pyw --filename_re $filename_re --background_tracks_dir $bg_tracks $key1 $key2 $key3 $dir1 $dir2
