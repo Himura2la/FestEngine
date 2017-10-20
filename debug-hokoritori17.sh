97ef86a15c3be183156cbdc4ea0f75884c65a8bf
Himura Kazuto <glagol15@gmail.com>
2017-10-20 23:32:37 +0300

    deleted runscripts

--- debug-hokoritori17.sh
+++
@@ -1,7 +0,0 @@
-cd src
-python3 main.pyw \
-    --filename_re "^(?P<num>\d{3}) (?P<nom>.*?)\. (?P<nicks>.*?) - (?P<name>.*?)( \(ID(?P<id>\d{1,3})\))?$" \
---background_tracks_dir "/media/himura/Data/Фесты/Hokori Tori 2017/Fest/background" \
---background_zad_path "/media/himura/Data/Фесты/Hokori Tori 2017/Fest/ht17_bg.jpg" \
---auto_load_files --debug_output \
-"/media/himura/Data/Фесты/Hokori Tori 2017/Fest/zad" "/media/himura/Data/Фесты/Hokori Tori 2017/Fest/tracks" \
