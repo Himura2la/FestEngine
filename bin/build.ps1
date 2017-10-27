3437bd008aba94c1885816d5aa888fe1dbf862f7
Himura Kazuto <glagol15@gmail.com>
2017-10-28 02:02:07 +0300

    Update build.ps1

--- bin/build.ps1
+++ bin/build.ps1
@@ -5,5 +5,5 @@ param (
 echo "Using VLC installation: $vlc"
 
 cd ..\src
-pyinstaller -n FestEngine --distpath ..\bin --workpath ..\tmp --add-binary "$vlc\libvlc.dll;." --add-binary "$vlc\libvlccore.dll;." --add-binary "$vlc\plugins;plugins" --clean --windowed -y main.pyw
+& pyinstaller -n FestEngine --distpath ..\bin --workpath ..\tmp --add-binary "$vlc\libvlc.dll;." --add-binary "$vlc\libvlccore.dll;." --add-binary "$vlc\plugins;plugins" --clean --windowed -y main.pyw
 cd ..
