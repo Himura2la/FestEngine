43e6469107b7ccc8819bf448c97bea4844aa50f7
himura <himura@kampai-nb>
2017-11-04 16:02:59 +0300

    trying to fix linux build

---
+++ bin/rm_extra_libs.sh
@@ -0,0 +1,3 @@
+cd FestEngine
+rm -f libX*.so* libxcb*.so* libgcc_s.so.1 libstdc++.so.6 libgpg-error.so.0 libexpat.so.1 libz.so.1 libasound.so.2 libICE.so.6 libSM.so.6 libidn.so.11 libuuid.so.1 libglib-2.0.so.0 libgobject-2.0.so.0 libgio-2.0.so.0 libgdk_pixbuf-2.0.so.0 libfontconfig.so.1 libfreetype.so.6 libharfbuzz.so.0 libpangoft2-1.0.so.0 libpangocairo-1.0.so.0 libpango-1.0.so.0
+
