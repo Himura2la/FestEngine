796a66464bd249bb964585369aa5724b77a88df3
Himura Kazuto <glagol15@gmail.com>
2017-11-06 17:08:11 +0300

    no vlc in linux + libs cleaner

--- bin/build.py
+++ bin/build.py
@@ -8,38 +8,19 @@ sources_path = os.path.join('..', 'src')
 main_file = 'main.pyw'
 
 pyinst_flags = ['--clean', '--windowed', '-y', main_file]
+appimage_excludelist_url = 'https://raw.githubusercontent.com/AppImage/AppImages/master/excludelist'
 
 self_name = os.path.basename(sys.argv[0])
 print("--------------- %s started! ---------------" % self_name)
 
 pyinst_addbinary_sep = ';'
 no_vlc = len(sys.argv) == 2 and sys.argv[1] == '-novlc'
+vlc_binaries = []
+vlc_path = None
 
 if sys.platform.startswith('linux'):
     pyinst_addbinary_sep = ':'
     pyinst_flags.insert(0, '--strip')
-    if not no_vlc:
-        if len(sys.argv) == 4:
-            libvlc_path, libvlccore_path, vlc_plugins_path = sys.argv[1:4]
-        else:
-            try:
-                vlc_plugins_path = subprocess.check_output(['locate', '-n', '1', 'vlc/plugins']).strip()
-                vlc_plugins_path = vlc_plugins_path.decode().split('plugins', 1)[0] + 'plugins'
-                libvlc_path = subprocess.check_output(['locate', '-n', '1', 'libvlc.so']).strip().decode()
-                libvlccore_path = subprocess.check_output(['locate', '-n', '1', 'libvlccore.so']).strip().decode()
-            except subprocess.CalledProcessError:
-                vlc_plugins_path = ""
-
-        if not os.path.isdir(vlc_plugins_path) or \
-                not os.path.isfile(libvlc_path) or \
-                not os.path.isfile(libvlccore_path):
-            print("VLC not found. Try to run 'sudo updatedb' if you just installed it or pass the correct paths in arguments (%s [libvlc-path libvlccore-path vlc-plugins-dir])." % self_name)
-
-        vlc_binaries = {libvlc_path: '.',
-                        libvlccore_path: '.',
-                        vlc_plugins_path: 'vlc/plugins'}
-
-        print("Discovered VLC: \n- %s\n- %s\n- %s" % (libvlc_path, libvlccore_path, vlc_plugins_path))
 
 elif sys.platform == "win32" and not no_vlc:
     if len(sys.argv) == 2:
@@ -62,12 +43,17 @@ elif sys.platform == "win32" and not no_vlc:
             vlc_binaries = {os.path.join(vlc_path, src): tgt for src, tgt in vlc_binaries.items()}
         if not vlc_path or not os.path.isdir(vlc_path):
             import platform
-            print("VLC not found. Install the %s VLC or pass the path to VLC as a parameter." % platform.architecture()[0])
+
+            print("VLC not found. Install the %s VLC or pass the path to VLC as a parameter." %
+                  platform.architecture()[0])
             exit(1)
     print("Using VLC installation: %s" % vlc_path)
 
-vlc_binaries = [] if no_vlc else sum([['--add-binary', '%s%s%s' % (src_path, pyinst_addbinary_sep, tgt_path)]
-                                        for src_path, tgt_path in vlc_binaries.items()], []) 
+    vlc_binaries = sum([['--add-binary', '%s%s%s' % (src_path, pyinst_addbinary_sep, tgt_path)]
+                        for src_path, tgt_path in vlc_binaries.items()], [])
+else:
+    print("Sorry, your platform is not supported")
+    exit(1)
 dist_path = os.path.abspath(os.curdir)
 os.chdir(sources_path)
 
@@ -85,3 +71,16 @@ while p.poll() is None:
         print("--- Still building... %ds passed." % t)
 
 print("--------------- Built in %ds with exitcode %d! ---------------" % (t, p.poll()))
+
+if p.poll() == 0 and sys.platform.startswith('linux'):
+    print("--- Cleaning extra libs according to the AppImage excludelibs list...")
+    import urllib.request
+    exclude_libs = urllib.request.urlopen(appimage_excludelist_url).read()
+    exclude_libs = [e.rsplit('.so', 1)[0] for e in exclude_libs.decode().split('\n') if e and e[0] != '#']
+    os.chdir(name)
+    all_files = os.listdir()
+    libs_to_exclude = filter(lambda f: any([f.startswith(l) for l in exclude_libs]), all_files)
+    print("--- Removing:", libs_to_exclude)
+    [os.remove(lib) for lib in libs_to_exclude]
+
+print("--------------- Ready! ---------------")
