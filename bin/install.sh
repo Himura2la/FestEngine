da35cf857778cad9f7b17d6d8acb85ab45176555
Roman Chistokhodov <freeslave93@gmail.com>
2017-11-08 02:43:53 +0300

    Fix install.sh to no longer use locate. Remove bashisms.

--- bin/install.sh
+++ bin/install.sh
@@ -1,35 +1,26 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ ! $1 == "" ]]; then
-    LIB_NAME=`basename $1`
-    echo "$LIB_NAME: "
+if [ -n "$1" ]; then
+    LIB_NAME=$(basename "$1")
+    printf "%s" "$LIB_NAME: "
 
-    if [[ $LIB_NAME == *"wx"* || $LIB_NAME == *"python"* ]]; then
-        echo Protected lib, skipping
-        echo
-        exit 0
-    fi
-    
-    LOCATE_OUT=`locate $LIB_NAME`
-    
-    if [[ $LOCATE_OUT == *"/lib/"* ]]; then
-        echo $LOCATE_OUT
-        echo Removing
-        rm $LIB_NAME
+    case "$LIB_NAME" in
+        *wx*) echo "Protected lib, skipping"; return 0 ;;
+        *python*) echo "Protected lib, skipping"; return 0 ;;
+        *) ;;
+    esac
+
+    LOCATE_OUT="$(/sbin/ldconfig -p | grep "$LIB_NAME" | tr ' ' '\n' | grep / | head -n 1)"
+
+    if [ -e "$LOCATE_OUT" ]; then
+        echo "Found on the system: $LOCATE_OUT. Removing $1"
+        rm -f "$1"
     else
-        echo Not found
+        echo "$LIB_NAME not found, keeping the bundled one"
     fi
 
-    echo
-    exit 0
+    return 0
 fi
 
-echo This script removes the libraries that you already have in your system. 
-echo Trying to run \'updatedb\'. This can take a while in case of success.
-updatedb
-if [[ $? -ne 0 ]]; then
-    echo "Failed to run 'updatedb'. The installation may be better if you run me as a superuser :3 (sudo $0)"
-fi
-
-find . -maxdepth 1 -type f -name 'lib*' -exec $0 '{}' \;
+find . -maxdepth 1 -type f -name 'lib*' -exec "$0" '{}' \;
 exit 0
